1ff836090f9822faa48ad7788722d1895d7531d1
2_faces <139215367+IvyLeagueCoder@users.noreply.github.com>
2025-12-07 15:02:46 +0700

    Thêm update function vào nhankhau controller

--- backend/controllers/nhaukhau.js
+++ backend/controllers/nhaukhau.js
@@ -13,4 +13,44 @@ export const getNhanKhau = (req,res) => {
             }
         }
     )
+} 
+
+export const _update = (req,res) => {
+    const {id,hoten,bietdanh,ngaysinh,gioitinh,noisinh,nguyenquan,diachi,dantoc,tongiao,quoctich,hochieu,nghenghiep,noilamviec,cccd,ngaycap,noicap,ngaychuyenden,noithuongtrutruoc,quanhe,mahokhau} = req.body
+
+    const query = 
+        'UPDATE "nhankhau" SET "hoten"=$1,"bietdanh"=$2,"ngaysinh"=$3,"gioitinh"=$4,"noisinh"=$5,"nguyenquan"=$6,"diachi"=$7,"dantoc"=$8,"tongiao"=$9,"quoctich"=$10,"hochieu"=$11,"nghenghiep"=$12,"noilamviec"=$13,"cccd"=$14,"ngaycap"=$15,"noicap"=$16,"ngaychuyenden"=$17,"noithuongtrutruoc"=$18,"quanhe"=$19,"mahokhau"=$20 WHERE "id"=$21'
+    db.query(
+        query,
+        [
+            hoten,
+            bietdanh,
+            ngaysinh,
+            gioitinh,
+            noisinh,
+            nguyenquan,
+            diachi,
+            dantoc,
+            tongiao,
+            quoctich,
+            hochieu,
+            nghenghiep,
+            noilamviec,
+            cccd,
+            ngaycap,
+            noicap,
+            ngaychuyenden,
+            noithuongtrutruoc,
+            quanhe,
+            parseInt(mahokhau),
+            parseInt(id)
+        ],
+        (error) => {
+            if(error) {
+                res.status(500).send("Lỗi server - updateNhanKhauById")
+            } else {
+                res.json("Cập nhật nhân khẩu thành công")
+            }
+        }
+    )
 }
