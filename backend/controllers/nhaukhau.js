63314d31af24ef06bf8edffb637053838158c05d
concaydanhram <hoang.bl235332@sis.hust.edu.vn>
2025-12-09 19:44:32 +0700

    them routes, add, delete nhan khau

--- backend/controllers/nhaukhau.js
+++ backend/controllers/nhaukhau.js
@@ -14,3 +14,56 @@ export const getNhanKhau = (req,res) => {
         }
     )
 }
+
+export const addNhanKhau = (req,res) => {
+    var {hoten,bietdanh,ngaysinh,gioitinh,noisinh,nguyenquan,diachi,dantoc,tongiao,quoctich,hochieu,nghenghiep,noilamviec,cccd,ngaycap,noicap,ngaychuyenden,noithuongtrutruoc,quanhe,mahokhau} = req.body
+    if(ngaycap==='')ngaycap = null;
+    if(ngaychuyenden==='')ngaychuyenden = null;
+    db.query(
+        'INSERT INTO nhankhau ("hoten","bietdanh","ngaysinh","gioitinh","noisinh","nguyenquan","diachi","dantoc","tongiao","quoctich","hochieu","nghenghiep","noilamviec","cccd","ngaycap","noicap","ngaychuyenden","noithuongtrutruoc","quanhe","mahokhau") VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20) RETURNING *',
+        [
+            hoten,
+            bietdanh,
+            ngaysinh,
+            gioitinh,
+            noisinh,
+            nguyenquan,
+            diachi,
+            dantoc,
+            tongiao,
+            quoctich,
+            hochieu,
+            nghenghiep,
+            noilamviec,
+            cccd,
+            ngaycap,
+            noicap,
+            ngaychuyenden,
+            noithuongtrutruoc,
+            quanhe,
+            parseInt(mahokhau)
+        ],
+        (error,results) => {
+            if(error) {
+                res.status(500).send(error.message)
+            } else {
+                res.json({status:"Thành công",nhankhau:results.rows[0]})
+            }
+        }
+    )
+}
+
+
+export const deleteNhanKhau = (req,res) => {
+    db.query(
+        'DELETE FROM nhankhau WHERE $1=0 OR "id"=ANY($2)',
+        [req.body.length,req.body],
+        (error) => {
+            if(error) {
+                res.status(500).send(error.message)
+            } else {
+                res.json("Xóa nhân khẩu thành công")
+            }
+        }
+    )
+}
