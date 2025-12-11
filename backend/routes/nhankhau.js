2c5f8ce8471380cd9c0902dedcfb51b6f150c7db
2_faces <139215367+IvyLeagueCoder@users.noreply.github.com>
2025-12-11 09:51:54 +0700

    Add detail route

--- backend/routes/nhankhau.js
+++ backend/routes/nhankhau.js
@@ -6,6 +6,7 @@ const router = express.Router()
 router.post("/get",getNhanKhau) // lấy nhân khẩu
 router.post("/put",_update) // cập nhật nhân khẩu
 router.post("/add",addNhanKhau) // thêm nhân khẩu
-router.post("/delete",deleteNhanKhau) // xóa nhân khẩu
+router.post("/delete",deleteNhanKhau) // xóa nhân khẩu 
+router.post("/detail",getdetail) //lấy thông tin chi tiết
 
 export default router
