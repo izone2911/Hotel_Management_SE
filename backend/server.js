1e157278c9fd0d4f3c7318e6f1fc47384ec2e213
Hadanh0703 <haducanhyb0703@gmail.com>
2025-12-25 08:34:31 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-//
-//import khoanthuRoutes from './routes/khoanthu.js';
-//app.use("/api/khoanthu", khoanthuRoutes);
-// 
-//import noptienRoutes from './routes/noptien.js';
-//app.use("/api/noptien", noptienRoutes)
+import khoanthuRoutes from "./routes/khoanthu.js";
+app.use("/api/khoanthu", khoanthuRoutes);
+
+import noptienRoutes from "./routes/noptien.js";
+app.use("/api/noptien", noptienRoutes);
 //
 import authRoutes from "./routes/auth.js";
 app.use("/api/auth", authRoutes);
@@ -19,24 +18,22 @@ app.use("/api/auth", authRoutes);
 //app.use("/api/users", userRoutes);
 //
 
-import nhankhauRoutes from "./routes/nhankhau.js"
-import hokhauRoutes from "./routes/hokhau.js"
-import khaituRoutes from './routes/khaitu.js'
-import tamvangRoutes from './routes/tamvang.js'
+import nhankhauRoutes from "./routes/nhankhau.js";
+import hokhauRoutes from "./routes/hokhau.js";
+import khaituRoutes from "./routes/khaitu.js";
+import tamvangRoutes from "./routes/tamvang.js";
 //import tamtruRoutes from './routes/tamtru.js'
 //import thongkeRoutes from './routes/thongke.js'
 
-app.use("/api/nhankhau",nhankhauRoutes)
-app.use("/api/hokhau",hokhauRoutes)
-app.use("/api/khaitu", khaituRoutes)
-app.use('/api/tamvang',tamvangRoutes)
+app.use("/api/nhankhau", nhankhauRoutes);
+app.use("/api/hokhau", hokhauRoutes);
+app.use("/api/khaitu", khaituRoutes);
+app.use("/api/tamvang", tamvangRoutes);
 //app.use("/api/tamtru",tamtruRoutes)
 //app.use('/api/thongke', thongkeRoutes)
 
-app.get('*', (req, res) => res.send('404 NOT FOUND!!!'))
+app.get("*", (req, res) => res.send("404 NOT FOUND!!!"));
 
 app.listen(4000, () => {
-  console.log("Connected!");
+    console.log("Connected!");
 });
-
-
