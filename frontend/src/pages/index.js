a7ba90a6d639bffd427618699479548875627cac
~ <huyhuytranhuu@gmail.com>
2026-01-03 11:15:48 +0700

    feat: add account manager ui

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Login } from './login'
 //export { default as Dashboard} from './dashboard'
-//export { default as Account } from './account'
+export { default as Account } from './account'
 export { default as KhoanThu } from './khoanthu'
 export { default as NhanKhau } from './nhankhau'
 export { default as NopTien } from './noptien'
